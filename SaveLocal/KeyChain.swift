c5949b74083e847dd5d4c0676251fe28c540453e
Bill Mårtensson <bill@magictechnology.se>
2020-10-08 21:55:51 +0200

    Start

---
+++ SaveLocal/KeyChain.swift
@@ -0,0 +1,75 @@
+//
+//  KeyChain.swift
+//  SaveLocal
+//
+//  Created by Bill Martensson on 2020-10-08.
+//
+
+import Foundation
+
+import Security
+
+/*
+ Example:
+ let int: Int = 555
+ let data = Data(from: int)
+ let status = KeyChain.save(key: "MyNumber", data: data)
+ print("status: ", status)
+
+ if let receivedData = KeyChain.load(key: "MyNumber") {
+     let result = receivedData.to(type: Int.self)
+     print("result: ", result)
+ }
+ */
+
+class KeyChain {
+
+    class func save(key: String, data: Data) -> OSStatus {
+        let query = [
+            kSecClass as String       : kSecClassGenericPassword as String,
+            kSecAttrAccount as String : key,
+            kSecValueData as String   : data ] as [String : Any]
+
+        SecItemDelete(query as CFDictionary)
+
+        return SecItemAdd(query as CFDictionary, nil)
+    }
+
+    class func load(key: String) -> Data? {
+        let query = [
+            kSecClass as String       : kSecClassGenericPassword,
+            kSecAttrAccount as String : key,
+            kSecReturnData as String  : kCFBooleanTrue!,
+            kSecMatchLimit as String  : kSecMatchLimitOne ] as [String : Any]
+
+        var dataTypeRef: AnyObject? = nil
+
+        let status: OSStatus = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+
+        if status == noErr {
+            return dataTypeRef as! Data?
+        } else {
+            return nil
+        }
+    }
+
+    class func createUniqueID() -> String {
+        let uuid: CFUUID = CFUUIDCreate(nil)
+        let cfStr: CFString = CFUUIDCreateString(nil, uuid)
+
+        let swiftString: String = cfStr as String
+        return swiftString
+    }
+}
+
+extension Data {
+
+    init<T>(from value: T) {
+        var value = value
+        self.init(buffer: UnsafeBufferPointer(start: &value, count: 1))
+    }
+
+    func to<T>(type: T.Type) -> T {
+        return self.withUnsafeBytes { $0.load(as: T.self) }
+    }
+}
--- SaveLocal/ViewController.swift
+++ SaveLocal/ViewController.swift
@@ -7,13 +7,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var theTextfield: UITextField!
+    
+    
+    @IBOutlet weak var theTableview: UITableView!
+    
+    var favorites = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let defaults = UserDefaults.standard
+        
+        /*
+        if let theSavedText = defaults.string(forKey: "thetext") {
+            theTextfield.text = theSavedText
+        }
+        */
+        
+        if let theSavedFav = defaults.array(forKey: "fav") {
+            favorites = theSavedFav as! [String]
+        }
+        
     }
+    
+    
+    @IBAction func letsSave(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        
+        //defaults.set(theTextfield.text, forKey: "thetext")
+        
+        favorites.append(theTextfield.text!)
+        
+        defaults.set(favorites, forKey: "fav")
+        
+        theTableview.reloadData()
+    }
+    
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        cell?.textLabel?.text = favorites[indexPath.row]
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        favorites.remove(at: indexPath.row)
+        
+        let defaults = UserDefaults.standard
+        defaults.set(favorites, forKey: "fav")
+        
+        theTableview.reloadData()
+    }
 }
+
 
